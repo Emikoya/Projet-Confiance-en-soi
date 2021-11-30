78a6f7feb61ad0bb7acb0948376356b6ec7dda54
Icatol <63586804+Icatol@users.noreply.github.com>
2021-11-30 17:15:54 +0100

    Add files via upload

--- projet.py
+++ projet.py
@@ -1,15 +1,25 @@
 import pygame
 from pygame.locals import *
+import time
+import random
 import sys
 
 pygame.init()
-screen = pygame.display.set_mode((400,300))
+screen = pygame.display.set_mode((400, 300))
 pygame.mouse.set_visible(False)
 pygame.display.set_caption("Photo")
 
-img = pygame.image.load("5.png")
-img = pygame.transform.scale(img,(400,300))
-rect_img = img.get_rect()
-screen.fill((0,0,0))
-screen.blit(img,rect_img)
-pygame.display.update()
+i = 1
+
+while (True):
+
+    img = pygame.image.load(str(random.randint(1, 20))+".png")
+    img = pygame.transform.scale(img, (400, 300))
+    rect_img = img.get_rect()
+    screen.fill((0, 0, 0))
+    screen.blit(img, rect_img)
+    pygame.display.update()
+    # if i == 20:
+    #     i = 1
+    # i = i+1
+    time.sleep(1)
