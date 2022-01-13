bc84a86dd66002e9c853cb22fb5752e6c6eea38a
Icatol <63586804+Icatol@users.noreply.github.com>
2022-01-13 13:13:38 +0100

    Update projet.py

--- projet.py
+++ projet.py
@@ -1,28 +1,37 @@
-#import
 import pygame
 from pygame.locals import *
 import time
 import random
 import sys
 
-#initialisation du cadre
 pygame.init()
-screen = pygame.display.set_mode((400, 300))
+screen = pygame.display.set_mode((400, 300))  # , FULLSCREEN
 pygame.mouse.set_visible(False)
-pygame.display.set_caption("Photo")
+pygame.display.set_caption("Confiance")
 
-i = 1
+image = random.randint(1, 20)
 
-#affichage aléatoire
 while (True):
 
-    img = pygame.image.load(str(random.randint(1, 20))+".png")
+    img = pygame.image.load(".\img\\"+str(image)+".png")
     img = pygame.transform.scale(img, (400, 300))
+
     rect_img = img.get_rect()
     screen.fill((0, 0, 0))
     screen.blit(img, rect_img)
     pygame.display.update()
-    # if i == 20:
-    #     i = 1
-    # i = i+1
+
+    # start = time.time()
+    # finish = start+4
+    # while time.time() < finish:
+    #     pass
     time.sleep(1)
+
+    diff = True
+    while (diff):
+        verifImage = image
+        image = random.randint(1, 20)
+        if verifImage != image:
+            image = random.randint(1, 20)
+        else:
+            diff = False
